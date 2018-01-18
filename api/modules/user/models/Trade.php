a82cfbf4269376839746e80c43b0d0c6dc51dd50
zhouyang <zhouyang@hoolai.com>
2018-01-18 16:07:32 +0800

    修改划转记录

--- api/modules/user/models/Trade.php
+++ api/modules/user/models/Trade.php
@@ -18,4 +18,30 @@ class Trade extends \common\models\CenterBridge
         return $model->save();
     }
 
+    //查询txid是否存在
+    public static function getTxidInfo($txid)
+    {
+        return Trade::find()->select("*")->where(["app_txid" => $txid])->asArray()->one();
+    }
+
+    //返回划转记录
+    public static function getList($address, $page, $pageSize)
+    {
+        $query = CenterBridge::find();
+        $query->where(["address" => $address]);
+        $query->orderBy('addtime DESC');
+        //分页
+        $count = $query->count();
+        $offset = ($page - 1) * $pageSize;
+        $query->offset($offset)->limit($pageSize);
+        $index_list = $query->asArray()->all();
+        //默认无下一页
+        $is_next_page = "0";
+        if ($count - ($page * $pageSize) >= 0) {
+            $is_next_page = "1";//有下一页
+        }
+        return ['list' => $index_list, 'is_next_page' => $is_next_page,"count"=>$count];
+    }
+
+
 }
