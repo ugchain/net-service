a779b522f0da987c470dc89a38aa1a97cd4b9ab8
sunfeng <sunfeng0712@163.com>
2018-01-24 01:04:47 +0800

    edit ug_sign_url

--- environments/test/common/config/params-local.php
+++ environments/test/common/config/params-local.php
@@ -5,7 +5,7 @@ return [
         "gas_price"=>0,
         "gas_limit"=>150000,
         "ug_host"    => "http://47.104.166.51:22000",
-        "ug_sign_url" =>"http://118.190.137.150:10000/ug/defreezeByVote",//ug签名
+        "ug_sign_url" =>"http://118.190.137.150:10000/ug/defreeze",//ug签名
         "owner_address" => "0x3a96700a6cce699c8219332202eca67b1442fbe1",
     ],
     "eth"=>[
