abd08f56bed9d923410eab08aa5cc9dffaa9aed8
sunfeng <sunfeng0712@163.com>
2018-01-30 19:34:37 +0800

    add create-packet

--- api/modules/redpacket/controllers/RedpacketController.php
+++ api/modules/redpacket/controllers/RedpacketController.php
@@ -24,6 +24,31 @@ class RedpacketController extends  Controller
 //            ]
 //        ];
 //    }
+    /**
+     * 创建红包
+     */
+    public function actionCreatePacket()
+    {
+        //红包标题
+        $title = Yii::$app->request->post("title", "");
+        //主题ID
+        $theme_id = Yii::$app->request->post("theme_id", "");
+        //地址
+        $address = Yii::$app->request->post("address", "");
+        //金额
+        $amount = Yii::$app->request->post("amount", "");
+        //个数
+        $quantity = Yii::$app->request->post("quantity", "");
+        //红包类型
+        $type = Yii::$app->request->post("type", "0");
+        //离线签名
+        $raw_transaction = Yii::$app->request->post("raw_transaction", "");
+        //hash
+        $hash = Yii::$app->request->post("hash", "");
+
+        //判断
+
+    }
 
         /**
          * 红包兑换
