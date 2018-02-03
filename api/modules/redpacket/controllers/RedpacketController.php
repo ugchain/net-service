6908e2910c3dbb2676285b29204d1d6a58b354a0
zhouyang <zhouyang@hoolai.com>
2018-02-03 15:59:53 +0800

    红包兑换接口增加返回值

--- api/modules/redpacket/controllers/RedpacketController.php
+++ api/modules/redpacket/controllers/RedpacketController.php
@@ -230,7 +230,7 @@ class RedpacketController extends  Controller
                 outputHelper::ouputErrorcodeJson(\common\helpers\ErrorCodes::FALL);
             }
 
-            outputHelper::ouputErrorcodeJson(\common\helpers\ErrorCodes::SUCCESS, ['id' => $result['rid']]);
+            outputHelper::ouputErrorcodeJson(\common\helpers\ErrorCodes::SUCCESS, ['id' => $result['rid'], 'amount' => $result['amount']]);
         } else {
             outputHelper::ouputErrorcodeJson(\common\helpers\ErrorCodes::SUCCESS, ['id' => $redPacketInfo['id'], 'title' => $redPacketInfo['title']]);
         }
