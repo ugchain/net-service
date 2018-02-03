e826cb1058450c04027fb344d923c3023ded6566
崔思楠 <cuisinan@datbc.com>
2018-02-03 16:28:04 +0800

    清空微信缓存

--- api/modules/redpacket/views/we-chat-red-packet/share.php
+++ api/modules/redpacket/views/we-chat-red-packet/share.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1, user-scalable=no">
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
     <title>红包领取</title>
     <link rel="stylesheet" href="/css/weui.min.css">
     <link rel="stylesheet" href="/css/jquery-weui.min.css">
