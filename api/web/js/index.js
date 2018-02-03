fedc8f6b93601a14664844645c2d20cf98ac0e57
崔思楠 <cuisinan@datbc.com>
2018-02-03 15:35:54 +0800

    修改复制口令功能

--- api/web/js/index.js
+++ api/web/js/index.js
@@ -54,15 +54,7 @@ if(state == 0){
             success: function(data){
                 if(data.code == 0){
                 	mask()
-					$('.received').show().css('opacity','0')
-					$('#kl-txt').val(data.data.code)
-					$('.btn').click()
-                    // var clipboard2 = new Clipboard('.btn2', {
-                    //     text: function() {
-                    //         return 'to be or not to be';
-                    //     }
-                    // });
-                    // $('.btn2').click()
+					$('#mask-copy').val(data.data.code)
                     $.toast(data.message, "text");
                 }
             },
