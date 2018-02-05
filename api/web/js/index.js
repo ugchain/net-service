d1fc9960b73395151a0cdee37aa22cd4f9d58b01
崔思楠 <cuisinan@datbc.com>
2018-02-05 11:47:07 +0800

    等待后台配置 接入wx_sdk

--- api/web/js/index.js
+++ api/web/js/index.js
@@ -1,165 +1,159 @@
-// 复制
-var clipboard = new Clipboard('.btn');
-clipboard.on('success', function(e) {
-    $.toast("复制成功", "text");
-    console.log(e)
-});
+$(function(){
+	// 复制
+	var clipboard = new Clipboard('.btn');
+	clipboard.on('success', function(e) {
+	    $.toast("复制成功", "text");
+	    console.log(e)
+	});
 
-clipboard.on('error', function(e) {
-    $.toast("复制失败", "text");
-    console.log(e)
-});
+	clipboard.on('error', function(e) {
+	    $.toast("复制失败", "text");
+	    console.log(e)
+	});
+
+	console.log(state)
+
+	// mask
+	function mask(){
+		$('.mask').show()
+		.find('.close').on('click', function(){
+			$('.mask').hide()
+			window.location.reload()
+		})
+	}
+
+	// 未领取
+	if(state == 0){
+		$('.tie').css({
+			'margin-top': '-30px'
+		})
+		$('.top').css({
+			"background": "url('/img/unreceived.png') no-repeat",
+			"background-size": '100%',
+		})
+		$('.unreceived').show()
+		$('.received').css({
+			'height': '0',
+			"padding-bottom": '0',
+			"padding-top": '0'
+		})
+
+		$('.packet-pic-open').hide()
+			.siblings('.get-ugc').hide()
+			.siblings('.packet-pic').show()
+		$('.state-info').hide()
+
+		// 获取口令 传入微信用户信息
+		$('#get-packet-btn').on('click',function(){
+	        $.ajax({
+	            url: 'grad-a-redpacket',
+	            type: 'post',
+	            dataType: 'json',
+	            data: {
+	                rid: rid,
+	                openid: openid,
+	                nickname: nickname,
+	                headimgurl: headimgurl,
+	                expire_time: expire_time
+	            },
+	            success: function(data){
+	                if(data.code == 0){
+	                	mask()
+	                	$('#mask-copy').val(data.data.code)
+	                }
+	            },
+	            error: function() {
+	            	$.toast("您的网络有问题", "text");
+	            }
+	        })
+		})
+
+	}
+
+	// 已领取
+	if(state == 1){
+		$('.received').show().css('opacity','1')
+		$('.packet-pic-open').hide()
+			.siblings('.get-ugc').hide()
+			.siblings('.packet-pic').show()
+
+		$('.state-info').css({
+			'font-size':'21px',
+			'color': '#fff'
+		}).text('恭喜您抢到一个UGC红包').show()
+	}
+
+	// 已兑换
+	if(state == 2){
+		$('.exchanged').show()
+		$('.received').hide()
+	}
+
+	// 已领光
+	if(state == 3){
+		$('.finished').show()
+		$('.received').hide()
+		$('.packet-pic-open').hide()
+			.siblings('.get-ugc').hide()
+			.siblings('.packet-pic').show()
+		$('.state-info').show()
+		$('.state-info').find('.state-tips')
+			.show()
+			.text('红包已领光')
+			.siblings('').hide()
+	}
+
+	// 已结束
+	if(state == 4){
+		$('.finished').show()
+		$('.received').hide()
+		$('.packet-pic-open').hide()
+			.siblings('.get-ugc').hide()
+			.siblings('.packet-pic').show()
+		$('.state-info').css({
+				'margin-top': '10px'
+			}).find('.state-time')
+				.text('于1-25 16:32 结束')
+				.siblings().hide()
+		$('.state-info').find('.state-tips')
+			.show()
+			.text('红包已结束')
+	}
+
+	// 模拟 加载领取详情
+	setTimeout(function() {
+		
+	}, 1500); 
 
 
+	// 配置sdk
+	// wx.ready(function () {
+	// 	wx.onMenuShareTimeline({
+	// 	    title: '', 
+	// 	    link: '', 
+	// 	    imgUrl: '', 
+	// 	    success: function () {
+		   
+	// 		},
+	// 		cancel: function () {
+			    
+	// 		}
+	// 	});
 
-console.log(state)
-// mask
-function mask(){
-	$('.mask').show()
-	.find('.close').on('click', function(){
-		$('.mask').hide()
-		window.location.reload()
-	})
-}
+	// 	wx.onMenuShareAppMessage({
+	// 		title: '', 
+	// 		desc: '', 
+	// 		link: '', 
+	// 		imgUrl: '', 
+	// 		type: '', 
+	// 		dataUrl: '',
+	// 		success: function () {
+			
+	// 		},
+	// 		cancel: function () {
+			
+	// 		}
+	// 	})
+	// })
 
-// 未领取
-if(state == 0){
-	$('.tie').css({
-		'margin-top': '-30px'
-	})
-	$('.top').css({
-		"background": "url('/img/unreceived.png') no-repeat",
-		"background-size": '100%',
-	})
-	$('.unreceived').show()
-	$('.received').css({
-		'height': '0',
-		"padding-bottom": '0',
-		"padding-top": '0'
-	})
-
-	$('.packet-pic-open').hide()
-		.siblings('.get-ugc').hide()
-		.siblings('.packet-pic').show()
-	$('.state-info').hide()
-
-
-	$('#get-packet-btn').on('click',function(){
-        $.ajax({
-            url: 'grad-a-redpacket',
-            type: 'post',
-            dataType: 'json',
-            data: {
-                rid: rid,
-                openid: openid,
-                nickname: nickname,
-                headimgurl: headimgurl,
-                expire_time: expire_time
-            },
-            success: function(data){
-                if(data.code == 0){
-                	mask()
-                	$('#mask-copy').val(data.data.code)
-                }
-            },
-            error: function() {
-            	$.toast("您的网络有问题", "text");
-            }
-        })
-	})
-
-}
-
-// 已领取
-if(state == 1){
-	$('.received').show().css('opacity','1')
-	$('.packet-pic-open').hide()
-		.siblings('.get-ugc').hide()
-		.siblings('.packet-pic').show()
-
-	$('.state-info').css({
-		'font-size':'21px',
-		'color': '#fff'
-	}).text('恭喜您抢到一个UGC红包').show()
-}
-
-
-// 已兑换
-if(state == 2){
-	$('.exchanged').show()
-	$('.received').hide()
-}
-
-// 已领光
-if(state == 3){
-	$('.finished').show()
-	$('.received').hide()
-	$('.packet-pic-open').hide()
-		.siblings('.get-ugc').hide()
-		.siblings('.packet-pic').show()
-	$('.state-info').show()
-	$('.state-info').find('.state-tips')
-		.show()
-		.text('红包已领光')
-		.siblings('').hide()
-}
-
-// 已结束
-if(state == 4){
-	$('.finished').show()
-	$('.received').hide()
-	$('.packet-pic-open').hide()
-		.siblings('.get-ugc').hide()
-		.siblings('.packet-pic').show()
-	$('.state-info').css({
-			'margin-top': '10px'
-		}).find('.state-time')
-			.text('于1-25 16:32 结束')
-			.siblings().hide()
-	$('.state-info').find('.state-tips')
-		.show()
-		.text('红包已结束')
-}
-
-// 滚动加载
-var loading = false;  
-var str =  '<a href="javascript:void(0);" class="weui-media-box weui-media-box_appmsg">'
-	str += '<div class="weui-media-box__hd">'
-	str += '<img class="weui-media-box__thumb" src="">'
-	str += '</div>' 
-	str +=	' <div class="weui-media-box__bd">'      
-	str +=	' <h4 class="weui-media-box__title">标题</h4>'        
-	str +=	' <p class="weui-media-box__desc">'        
-	str +=  ' 我是加载出来的。'
-	str +=  '</p>'
-	str +=	'</div>'     
-	str +=	'</a>'    
-
-// $('body').infinite().on("infinite", function() {
-// 	if(loading) return;
-// 	loading = true;
-
-// 	// $.ajax({
-// 	// 	url: '/path/to/file',
-// 	// 	type: 'post',
-// 	// 	dataType: 'json',
-// 	// 	data: {
-// 	// 		param1: 'value1',
-// 	// 		param1: 'value1',
-// 	// 	},
-// 	// 	success: function(){
-// 	// 		$(".weui-panel__bd").append(str);
-// 	// 		loading = false;
-// 	// 	},
-// 	// 	error(function() {
-// 	// 		loading = false;
-// 	// 		$('.weui-loadmore__tips').text('加载失败')
-// 	// 	})
-// 	// })
-
-// 	// 模拟
-// 	setTimeout(function() {
-// 	$(".weui-panel__bd").append(str);
-// 	loading = false;
-// 	}, 1500); 
-// });
+})
