be09d95ebcdea5451bb11dfaa24a72146660cad9
sunfeng <sunfeng0712@163.com>
2018-02-05 20:54:05 +0800

    update create red

--- api/modules/redpacket/controllers/RedpacketController.php
+++ api/modules/redpacket/controllers/RedpacketController.php
@@ -98,12 +98,12 @@ class RedpacketController extends  Controller
         if($block_info){
             $block_info = json_decode($block_info,true);
             //blockNumber 不为空
-            if(!isset($block_info["error"]) && $block_info["blockNumber"] != null){
+            if(!isset($block_info["error"]) && $block_info["result"]["blockNumber"] != null){
                 //检测上链成功,更新红包状态为status=2 && ug_trade 交易记录改为交易成功
                 RedPacket::updateStatus($packet_id,"2");
                 //blockNumber截取前两位0x && 16进制 转换为10进制
-                $block_info = Operating::substrHexdec($block_info);
-                Trade::updateBlockAndStatusBytxid($data["hash"], $block_info["blockNumber"], Trade::SUCCESS);
+                $trade_info = Operating::substrHexdec($block_info["result"]);
+                Trade::updateBlockAndStatusBytxid($data["hash"], $trade_info["blockNumber"], Trade::SUCCESS);
                 $this->REPACK_STATUS = 1;
             }
         }
