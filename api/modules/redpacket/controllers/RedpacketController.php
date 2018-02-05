1c29a70b43aa2509a1d9e7d3983df7178a423d26
sunfeng <sunfeng0712@163.com>
2018-02-05 23:43:03 +0800

    add listen detail for ug chain

--- api/modules/redpacket/controllers/RedpacketController.php
+++ api/modules/redpacket/controllers/RedpacketController.php
@@ -258,6 +258,22 @@ class RedpacketController extends  Controller
 
         //获取红包详情数据
         $result = RedPacket::getRedPacketInfoWithRecordList($id);
+        if($result["status"] == 0){
+            //检测是否上链--成功5%
+            $block_info = CurlRequest::ChainCurl(Yii::$app->params["ug"]["ug_host"], "eth_getTransactionReceipt", [$result["hash"]]);
+            if($block_info){
+                $block_info = json_decode($block_info,true);
+                //blockNumber 不为空
+                if(!isset($block_info["error"]) && $block_info["result"]["blockNumber"] != null){
+                    //检测上链成功,更新红包状态为status=2 && ug_trade 交易记录改为交易成功
+                    RedPacket::updateStatus($result["id"],"2");
+                    //blockNumber截取前两位0x && 16进制 转换为10进制
+                    $trade_info = Operating::substrHexdec($block_info["result"]);
+                    Trade::updateBlockAndStatusBytxid($result["hash"], $trade_info["blockNumber"], Trade::SUCCESS);
+                    $result["status"] = "2";
+                }
+            }
+        }
 
         //返回数据
         outputHelper::ouputErrorcodeJson(\common\helpers\ErrorCodes::SUCCESS, $result);
