dd41ad665bec1cbaf41dc32af36fb8f77e9132ac
sunfeng <sunfeng0712@163.com>
2018-02-06 14:21:14 +0800

    code for bug

--- common/helpers/ErrorCodes.php
+++ common/helpers/ErrorCodes.php
@@ -38,11 +38,8 @@ class ErrorCodes
         ROSE_THEME_NOT_EXISTS                                       =   20119,//玫瑰主题未获取到
         RED_PACKET_QUANTITY_EXCEEDED                                =   20120,//红包数量超出限制
         RED_PACKET_TITLE_EXCEEDED                                   =   20121,//红包名称超出限制
-<<<<<<< HEAD
-        RED_PACKET_OPEN                                             =   20122;//您已经拆过这个红包啦
-=======
+        RED_PACKET_OPEN                                             =   20122,//您已经拆过这个红包啦
         RED_PACKET_SEND_CHAIN_FALL                                  =   20122;//上链失败
->>>>>>> 390b6213e12c00b1699b115134f942d87d7d226b
 
 
 
@@ -77,11 +74,8 @@ class ErrorCodes
         self::ROSE_THEME_NOT_EXISTS                                 =>'主题缺失',
         self::RED_PACKET_QUANTITY_EXCEEDED                          =>'红包数量超出限制',
         self::RED_PACKET_TITLE_EXCEEDED                             =>'红包名称超出限制',
-<<<<<<< HEAD
         self::RED_PACKET_OPEN                                       =>'您已经拆过这个红包啦',
-=======
         self::RED_PACKET_SEND_CHAIN_FALL                            =>'发送交易上链失败',
->>>>>>> 390b6213e12c00b1699b115134f942d87d7d226b
     ];
 
 }
